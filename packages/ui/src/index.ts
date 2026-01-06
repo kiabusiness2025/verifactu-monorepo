79cf94c11dcbb18081cc4d4f3ac38345d1253179
Ksenia Ilicheva <kiabusiness2025@gmail.com>
2026-01-06 15:42:23 +0100

    Fix(ui): export Alert/Modal/Dropdown types

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,23 +1,33 @@
+// Button
 export { Button } from "./button/Button";
 export type { ButtonProps } from "./button/Button";
 
-export { Modal } from "./modal/index";
-export type { ModalProps } from "./modal/index";
-
+// Alert
 export { Alert } from "./alert/Alert";
 export type { AlertProps } from "./alert/Alert";
 
-export { Avatar } from "./avatar/Avatar";
-export type { AvatarProps } from "./avatar/Avatar";
+// Modal
+export { Modal } from "./modal";
+export type { ModalProps } from "./modal";
 
-export { Badge } from "./badge/Badge";
-export type { BadgeProps } from "./badge/Badge";
-
+// Dropdown
 export { Dropdown } from "./dropdown/Dropdown";
+export type { DropdownProps } from "./dropdown/Dropdown";
+
 export { DropdownItem } from "./dropdown/DropdownItem";
 export type { DropdownItemProps } from "./dropdown/DropdownItem";
 
+// Avatar
+export { Avatar } from "./avatar/Avatar";
+export type { AvatarProps } from "./avatar/Avatar";
+
+// Badge
+export { Badge } from "./badge/Badge";
+export type { BadgeProps } from "./badge/Badge";
+
+// Logo
 export { Logo } from "./logo/Logo";
 
-export { Table, TableHeader, TableBody, TableRow, TableCell } from "./table/index";
-export type { TableProps, TableHeaderProps, TableBodyProps, TableRowProps, TableCellProps } from "./table/index";
+// Table
+export { Table, TableHeader, TableBody, TableRow, TableCell } from "./table";
+export type { TableProps, TableHeaderProps, TableBodyProps, TableRowProps, TableCellProps } from "./table";
