2c8514956d53d60404482b5acbd91b7b97b3c727
Ksenia Ilicheva <kiabusiness2025@gmail.com>
2026-01-22 18:50:15 +0100

    fix(app): remove dropdown tag prop

--- apps/app/components/ecommerce/MonthlyTarget.tsx
+++ apps/app/components/ecommerce/MonthlyTarget.tsx
@@ -94,14 +94,12 @@ export default function MonthlyTarget() {
               className="w-40 p-2"
             >
               <DropdownItem
-                tag="a"
                 onItemClick={closeDropdown}
                 className="flex w-full font-normal text-left text-gray-500 rounded-lg hover:bg-gray-100 hover:text-gray-700 dark:text-gray-400 dark:hover:bg-white/5 dark:hover:text-gray-300"
               >
                 View More
               </DropdownItem>
               <DropdownItem
-                tag="a"
                 onItemClick={closeDropdown}
                 className="flex w-full font-normal text-left text-gray-500 rounded-lg hover:bg-gray-100 hover:text-gray-700 dark:text-gray-400 dark:hover:bg-white/5 dark:hover:text-gray-300"
               >
